7daf1efea5f889f1f871f39fa728bc9b37fc4014
Matthew <m_kmorgan@hotmail.com>
2020-09-27 19:18:43 -0600

    Mid-way through #10

--- src/recursion.js
+++ src/recursion.js
@@ -157,6 +157,11 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (reverse(string) === string) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
